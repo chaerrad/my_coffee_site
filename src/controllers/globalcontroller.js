5d0f5c5d43ab981eec2f42ca61c9bb77f393fd84
byuengyun Chae <chaeby7@gmail.com>
2022-01-25 00:01:32 +0900

    Update mongo model and mongoose

--- src/controllers/globalcontroller.js
+++ src/controllers/globalcontroller.js
@@ -1,3 +1,9 @@
+import User from "../models/loginmodels";
 export const handleHome =(req,res) => {
+    const {ID,password}=req.body;
+    let user = await User.create({
+        ID,
+        password
+    });
     return res.render("home.pug");
 }
--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
+mongoose.connect("mongodb://127.0.0.1:27017/my_coffee", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
