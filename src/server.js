2d0727118b6fa457e36b36fe68a20ccdadef59ba
byuengyun Chae <chaeby7@gmail.com>
2022-02-21 22:15:34 +0900

    Cookies and Sessions

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import "./db";
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import "./models/loginmodels";
 import globalRouter from "./routers/globalRouter.js";
 import loginRouter from "./routers/loginRouter.js";
@@ -17,6 +19,15 @@ app.use(express.urlencoded({ extended: true }));
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 app.listen(PORT, handleListening);
+
+app.use(
+  session({
+     secret: "Hello",
+     resave: true,
+     saveUninitialized: true,
+     store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/my_coffee"}),
+  })
+);
 
 app.use("/", globalRouter);
 app.use("/login", loginRouter);
